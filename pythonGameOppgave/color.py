24bf2bf5ebc1d85e6051ffa4b3a064efcfa9c162
DenniHeimDev <denniheim86@gmail.com>
2022-02-25 21:16:13 +0100

    setup of window

--- pythonGameOppgave/color.py
+++ pythonGameOppgave/color.py
@@ -1,9 +1,9 @@
 class color():
-    black = [0, 0, 0]
-    white = [255, 255, 255]
-    red = [255, 0, 0]
-    orange = [255, 102, 0]
-    yellow = [255, 255, 0]
-    green = [0, 255, 0]
-    blue = [0, 0, 255]
-    purple = [128, 0, 128]
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    orange = (255, 102, 0)
+    yellow = (255, 255, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    purple = (128, 0, 128)
--- pythonGameOppgave/main.py
+++ pythonGameOppgave/main.py
@@ -3,10 +3,27 @@ import color
 
 gameColor = color.color()
 
+FPS = 60
+clock = pygame.time.Clock()
+
 def main():
     pygame.init()
     
-    print(gameColor.white)
+    window()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+        clock.tick(FPS)
+
+
+def window():
+    # setup window 
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Pygame Window")
+
 
 if __name__ == '__main__':
     main()
